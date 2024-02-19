4b371164cb77a2aed3095998118dff103196dccb
Kuba <106245989+Corax0x01@users.noreply.github.com>
2024-02-19 21:25:29 +0100

    Initial commit

---
+++ src/main/java/szymanski/jakub/KsiazkaKucharska/KsiazkaKucharskaApplication.java
@@ -0,0 +1,13 @@
+package szymanski.jakub.KsiazkaKucharska;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsiazkaKucharskaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsiazkaKucharskaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/szymanski/jakub/KsiazkaKucharska/KsiazkaKucharskaApplicationTests.java
@@ -0,0 +1,13 @@
+package szymanski.jakub.KsiazkaKucharska;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsiazkaKucharskaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
