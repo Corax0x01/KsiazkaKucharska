1005bc387a2c82adeec2dc10313939a8da3974ef
Kuba <106245989+Corax0x01@users.noreply.github.com>
2024-02-22 01:16:46 +0100

    Refactor

--- src/main/java/szymanski/jakub/KsiazkaKucharska/domain/RecipeIngredient.java
+++ src/main/java/szymanski/jakub/KsiazkaKucharska/domain/RecipeIngredient.java
@@ -1,21 +1,19 @@
 package szymanski.jakub.KsiazkaKucharska.domain;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Entity
+@EqualsAndHashCode
 @Table(name = "recipe_ingredients")
 public class RecipeIngredient {
 
     @EmbeddedId
-    private RecipeIngredientKey id = new RecipeIngredientKey();
+    private RecipeIngredientKey id;
 
     @ManyToOne(cascade = CascadeType.ALL)
     @MapsId("recipeId")
