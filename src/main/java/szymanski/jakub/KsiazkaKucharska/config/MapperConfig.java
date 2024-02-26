f5e45c5f3fc5b949e325d550a158a1cd9585d290
Kuba <106245989+Corax0x01@users.noreply.github.com>
2024-02-26 23:44:51 +0100

    Update MapperConfig.java

--- src/main/java/szymanski/jakub/KsiazkaKucharska/config/MapperConfig.java
+++ src/main/java/szymanski/jakub/KsiazkaKucharska/config/MapperConfig.java
@@ -1,6 +1,7 @@
 package szymanski.jakub.KsiazkaKucharska.config;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,7 +10,10 @@ public class MapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+        // Allows request to create nested objects
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
+        return modelMapper;
     }
 
 }
