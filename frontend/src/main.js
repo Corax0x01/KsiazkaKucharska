fd44f04440db07b896438ad15b745c206cc2c58d
Kuba <106245989+Corax0x01@users.noreply.github.com>
2024-04-24 16:45:28 +0200

    Added font awesome to project

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,9 @@ import './assets/main.css'
 import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 
 createApp(App)
     .use(router)
+    .component('font-awesome-icon', FontAwesomeIcon)
     .mount('#app')
