527e7f1fcc181c595dcd53b96ab59eb668ba37cd
Kuba <106245989+Corax0x01@users.noreply.github.com>
2024-04-24 16:40:47 +0200

    Created check and delete buttons

---
+++ frontend/src/components/buttons/CheckButton.vue
@@ -0,0 +1,19 @@
+<script setup>
+import {faCheck} from "@fortawesome/free-solid-svg-icons";
+</script>
+
+<template>
+  <button class="btn btn-bd-check me-1">
+    <font-awesome-icon :icon="faCheck"/>
+  </button>
+</template>
+
+<style scoped>
+  .btn-bd-check {
+    --bs-btn-bg: var(--bs-green);
+    --bs-btn-hover-bg: #19875495;
+    --bs-btn-active-bg: #19875480;
+    --bs-btn-color: var(--bs-white);
+    --bs-btn-border-radius: 100%;
+  }
+</style>
---
+++ frontend/src/components/buttons/DeleteButton.vue
@@ -0,0 +1,19 @@
+<script setup>
+import {faTrash} from "@fortawesome/free-solid-svg-icons";
+</script>
+
+<template>
+  <button class="btn btn-bd-trash">
+    <font-awesome-icon :icon="faTrash"/>
+  </button>
+</template>
+
+<style scoped>
+  .btn-bd-trash {
+    --bs-btn-bg: var(--bs-red);
+    --bs-btn-hover-bg: #dc354595;
+    --bs-btn-active-bg: #dc354580;
+    --bs-btn-color: var(--bs-white);
+    --bs-btn-border-radius: 100%;
+  }
+</style>
