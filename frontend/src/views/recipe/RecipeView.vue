27f158d5a415d710f826bfedd7519edde8e16b65
Kuba <106245989+Corax0x01@users.noreply.github.com>
2024-04-26 18:17:51 +0200

    Moved function to RecipeService and added styling

--- frontend/src/views/recipe/RecipeView.vue
+++ frontend/src/views/recipe/RecipeView.vue
@@ -1,27 +1,44 @@
 <script setup>
   import {useRoute} from "vue-router";
   import {ref, onMounted} from "vue";
+  import RecipeService from "@/services/RecipeService.js";
+  import FileUploadService from "@/services/FileUploadService.js";
 
-  const route = useRoute()
-  const recipe = ref()
+  const route = useRoute();
+  const recipe = ref({});
+  const ingredients = ref([]);
+  const image = ref();
 
-  onMounted(() =>
-      fetch("/api/recipes/" + route.params.id)
-          .then((response) => response.json())
-          .then((data) => {
-            recipe.value = data;
-          })
-  )
+  onMounted(async () => {
+    const recipeId = route.params.id;
+    recipe.value = await RecipeService.getRecipeData(recipeId);
+    ingredients.value = await RecipeService.getRecipeIngredients(recipeId);
+    image.value = await FileUploadService.getFile(recipe.value.imageURL);
+  });
 </script>
 
 <template>
-  <div class="recipe">
-<!--    <img :src="recipe.imageURL" :alt="recipe.title">-->
-<!--    <h2>{{ recipe.title }}</h2>-->
-    <h2>Recipe info</h2>
+  <div class="recipe ps-3">
+    <img class="recipe-img" :src="image" :alt="recipe.title">
+    <h2>{{ recipe.title }}</h2>
+    <p class="recipe-description">{{recipe.description}}</p>
+    <ul class="ingredients">
+      <li v-for="ingredient in ingredients">{{ingredient.ingredient.name}}: {{ingredient.quantity}}</li>
+    </ul>
   </div>
 </template>
 
 <style scoped>
+  .recipe {
+    width: 66vw;
+  }
 
+  .recipe-img {
+    max-height: 20vw;
+  }
+
+  .ingredients {
+    list-style: none;
+    padding: 0;
+  }
 </style>
