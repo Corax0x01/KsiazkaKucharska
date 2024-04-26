a26c596f6977839c53ee866af8a59f72f8bda331
Kuba <106245989+Corax0x01@users.noreply.github.com>
2024-04-26 18:15:36 +0200

    Moved function to RecipeService

--- frontend/src/components/recipe/RecipeList.vue
+++ frontend/src/components/recipe/RecipeList.vue
@@ -1,20 +1,17 @@
 <script setup>
 import {onMounted, ref} from "vue";
   import RecipeItem from "@/components/recipe/RecipeItem.vue";
+  import RecipeService from "@/services/RecipeService.js";
 
-  const recipes = ref("asdah")
+  const recipes = ref([]);
 
-  // onMounted(() =>
-  //   fetch("/api/recipes")
-  //       .then((response) => response.json())
-  //       .then((data) => {
-  //         recipes.value = data;
-  //       })
-  // )
+  onMounted(async () => {
+    recipes.value = await RecipeService.getRecipesData();
+  });
 </script>
 
 <template>
-  <recipe-item v-if="recipes" v-for="recipe in recipes" :recipe="recipe"/>
+  <recipe-item v-if="recipes.length" v-for="recipe in recipes" :recipe="recipe"/>
   <div v-else>
     <h1 class="text-white-50 text-center align-content-center">There are no recipes in database</h1>
   </div>
