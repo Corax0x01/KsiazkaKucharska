89801245021e8d6e8e75e1f7a068fa1408d64d58
Kuba <106245989+Corax0x01@users.noreply.github.com>
2024-04-26 18:19:44 +0200

    Swapped css styling with bootstrap classes and added dynamic data

--- frontend/src/components/recipe/RecipeItem.vue
+++ frontend/src/components/recipe/RecipeItem.vue
@@ -1,54 +1,27 @@
 <script setup>
+  import {onMounted, ref} from "vue";
   import {useRouter} from "vue-router";
+  import FileUploadService from "@/services/FileUploadService.js";
 
   const { recipe } = defineProps(['recipe'])
 
   const router = useRouter()
+  const image = ref();
 
   const navigateToRecipe = () => {
     router.push(`/recipes/${recipe.id}`)
   }
+
+  onMounted(async () => {
+    image.value = await FileUploadService.getFile(recipe.imageURL);
+  })
 </script>
 
 <template>
-  <div :key="recipe.id" class="recipe-tile" @click="navigateToRecipe">
-    <img src="https://cdn.aniagotuje.com/pictures/articles/2022/01/23318621-v-1080x1080.jpg" alt="jajecznica">
-    <div class="recipe-text">
-      <h2>Jajecznica</h2>
+  <div class="card bg-dark me-2 mb-3 text-center" :key="recipe.id" @click="navigateToRecipe" style="width: 20%; cursor: pointer">
+    <img class="card-img-top" style="height: 90%" :src="image" :alt="recipe.title">
+    <div class="card-body">
+      <h5>{{ recipe.title }}</h5>
     </div>
-
-<!--    <img src=" {{ recipe.imageURL }}" alt="{{ recipe.title }}">-->
-<!--    <div class="recipe-text">-->
-<!--      <h2>{{ recipe.title }}</h2>-->
-<!--    </div>-->
   </div>
 </template>
-
-<style scoped>
-  .recipe-tile {
-    width: 256px;
-    overflow: hidden;
-    border-radius: 5%;
-    border: 1px solid rgba(0, 0, 0, 0.8);
-    box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
-    margin-bottom: 35px;
-    margin-right: 20px;
-    cursor: pointer;
-    background-color: dimgray;
-  }
-
-  .recipe-tile img {
-    width: 100%;
-    height: 190px;
-    margin: 0;
-  }
-
-  .recipe-tile .recipe-text {
-    padding: 0 5px;
-  }
-
-  .recipe-tile .recipe-text h2 {
-    font-weight: bold;
-    text-align: center;
-  }
-</style>
